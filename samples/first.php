30c4924c996abe849d99da93890636392401401e
Maxime BLAISE <maxime.blaise12@gmail.com>
2016-12-15 14:35:00 +0100

    Nettoyage

--- samples/first.php
+++ samples/first.php
@@ -28,8 +28,21 @@ $apiContext->setConfig(
 
 // After Step 3
 try {
-    $creditCard->create($apiContext);
-    echo $creditCard;
+    /*$creditCard->create($apiContext);
+    echo $creditCard;*/
+
+    $creditCard = new \PayPal\Api\CreditCard(
+    '{
+    "type": "visa",
+    "number": "4417119669820331",
+    "expire_month": "11",
+    "expire_year": "2019",
+    "cvv2": "012",
+    "first_name": "Joe",
+    "last_name": "Shopper"
+    }');
+    // This will print the number
+    echo $creditCard->getNumber();
 }
 catch (\PayPal\Exception\PayPalConnectionException $ex) {
     // This will print the detailed information on the exception. 
