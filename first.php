4320c8f26986da598ec5856a957cd563ea3311e4
Maxime BLAISE <maxime.blaise12@gmail.com>
2016-12-15 14:06:04 +0100

    Add Paypal API

---
+++ first.php
@@ -0,0 +1,38 @@
+<?php
+
+require __DIR__  . '/vendor/autoload.php';
+
+$apiContext = new \PayPal\Rest\ApiContext(
+    new \PayPal\Auth\OAuthTokenCredential(
+        'AYSq3RDGsmBLJE-otTkBtM-jBRd1TCQwFf9RGfwddNXWz0uFU9ztymylOhRS',     // ClientID
+        'EGnHDxD_qRPdaLdZz8iCr8N7_MzF-YHPTkjs6NKYQvQSBngp4PTTVWkPZRbL'      // ClientSecret
+    )
+);
+
+$creditCard = new \PayPal\Api\CreditCard();
+$creditCard->setType("visa")
+    ->setNumber("4417119669820331")
+    ->setExpireMonth("11")
+    ->setExpireYear("2019")
+    ->setCvv2("012")
+    ->setFirstName("Joe")
+    ->setLastName("Shopper");
+
+$apiContext->setConfig(
+  array(
+    'log.LogEnabled' => true,
+    'log.FileName' => 'PayPal.log',
+    'log.LogLevel' => 'DEBUG'
+  )
+);
+
+// After Step 3
+try {
+    $creditCard->create($apiContext);
+    echo $creditCard;
+}
+catch (\PayPal\Exception\PayPalConnectionException $ex) {
+    // This will print the detailed information on the exception. 
+    //REALLY HELPFUL FOR DEBUGGING
+    echo $ex->getData();
+}
